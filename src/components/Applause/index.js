a787c8e83fc663e16365957a654b47adbb23f4f2
yoko-at-home <yoko_iwasakijp@yahoo.co.jp>
2021-03-23 17:48:58 +0900

    ⚡️ changed button size and position

--- src/components/Applause/index.js
+++ src/components/Applause/index.js
@@ -11,17 +11,17 @@ function Applause() {
       </button>
       <style jsx>{`
         .btn {
-          border-radius: 5px;
+          border-radius:  50%;
           border: none;
           -moz-text-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
           -webkit-text-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
           -ms-text-shadow: 5px 5px 10px rgba(0, 0, 0, 0.5);
           text-shadow: 3px 3px 10px rgba(0, 0, 0, 1.9);
-          width: 45px;
-          height: 45px;
+          width: 43px;
+          height: 43px;
           font-size: 1rem;
           cursor: pointer;
-          background: linear-gradient(30deg, #fcab39, #d1c219);
+          background: transparent;
           box-shadow: 0px -6px 36px 0px #fbeb36, 0px 0px 6px 0px #ffb347;
         }
 
